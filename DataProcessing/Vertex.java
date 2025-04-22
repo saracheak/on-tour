94094144bb6d8c7c2a610bbeb2521334a1fe74ec
Svdark_win <63770618+xinkongwuyin@users.noreply.github.com>
2025-04-22 12:18:09 -0400

    Update Vertex.java
    add some variable

--- DataProcessing/Vertex.java
+++ DataProcessing/Vertex.java
@@ -1,28 +1,31 @@
-package DataProcessing;
-
 import java.util.ArrayList;
 public class Vertex{
-    int val;
-    int weight;
-    int numbersOfVertex;
-    ArrayList<Integer> nearbyVertex;
-    ArrayList<Integer> nearbyVertexValues;
+    private int val;
+    private int weight;
+    private int numbersOfVertex;
+    public ArrayList<Integer> nearbyVertex;
+    private boolean ifDiscoveried;
+    private int pointTo;
 
-    public Vertex(int val, int weight, int numbersOfVertex, ArrayList<Integer> nearbyVertex, ArrayList<Integer> nearbyVertexValues) {
+    public Vertex(int val, int weight, int numbersOfVertex, ArrayList<Integer> nearbyVertex) {
         this.val = val;
         this.weight = weight;
         this.numbersOfVertex = numbersOfVertex;
         this.nearbyVertex = nearbyVertex != null ? nearbyVertex : new ArrayList<>();
-        this.nearbyVertexValues = new ArrayList<>();
+        this.ifDiscoveried=false;
+        this.pointTo=-1;
     }
 
+    public void setVal (int val){this.val=val;}
+    public void setWeight(int weight){this.weight=weight;}
+    public void setNumbersOfVertex(int numbersOfVertex){this.numbersOfVertex=numbersOfVertex;}
+    public void setIfDiscoveried(){this.ifDiscoveried=!this.ifDiscoveried;}
+    public void setPointTo(int val){this.pointTo=val;}
 
     public int getVal() { return val; }
     public int getWeight() { return weight; }
+    public int getNumbersOfVertex() { return numbersOfVertex; }
     public ArrayList<Integer> getNearbyVertex() { return nearbyVertex; }
-
-    public ArrayList<Integer> getNearbyVertexValues(){
-        return nearbyVertexValues;
-    }
-
+    public boolean getIfDiscoveried(){return ifDiscoveried;}
+    public int getPointTo(){return pointTo;}
 }
